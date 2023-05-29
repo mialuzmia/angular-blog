b1745f995ad9b8257acb0953edccaeee8a1f780a
Mia Azevedo <mialuzazevedo@gmail.com>
2023-05-29 06:32:37 -0400

    add dynamic to components and home page completed

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-big-card',
-  templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+	selector: 'app-big-card',
+	templateUrl: './big-card.component.html',
+	styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent implements OnInit {
+	@Input()
+	bigCardPhoto:string = ''
+	@Input()
+	bigCardTitle:string = ''
+	@Input()
+	bigCardDescription:string = ''
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
